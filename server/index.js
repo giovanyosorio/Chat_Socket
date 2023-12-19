92dc404d5f21d6ce45c9ed518184337040567c8a
Giovany Osorio <30839218+giovanyosorio@users.noreply.github.com>
2023-12-19 21:26:04 +0000

    instalando morgan

--- server/index.js
+++ server/index.js
@@ -1,9 +1,12 @@
 import  express  from "express";
+import loger from "morgan";
+
 
 
 const app = express();
 const port= process.env.PORT ?? 3000;
 
+app.use(loger('dev'));
 
 app.get('/', (req, res) => {  
     res.send('<h1> Este es el chat</h1>');
