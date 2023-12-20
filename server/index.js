3b1de272a9d9a9e6c39b5cc761243ad3ea0c65fe
Giovany Osorio <30839218+giovanyosorio@users.noreply.github.com>
2023-12-20 17:08:12 +0000

    insertando datos en supabase

--- server/index.js
+++ server/index.js
@@ -3,6 +3,13 @@ import loger from "morgan";
 import {Server} from "socket.io";
 import { createServer } from "http";
 
+import { createClient } from '@supabase/supabase-js'
+import dotenv from "dotenv";
+
+const supabaseUrl = 'https://ghiorbbfudumyxigrlpd.supabase.co'
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdoaW9yYmJmdWR1bXl4aWdybHBkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDMwODY2OTIsImV4cCI6MjAxODY2MjY5Mn0.CsNlGgXORwNLHjMHjDrrtkQym_V2OcR1FqIe-y1aNZg"
+const supabase = createClient(supabaseUrl, supabaseKey)
+
 
 const app = express();
 const port= process.env.PORT ?? 3000;
@@ -11,16 +18,33 @@ const io= new Server(server,{
     connectionStateRecovery: { },
 });
 
+
+
 io.on('connection', (socket) => {
     console.log('a user connected');
-    socket.on('disconnect', () => {
-        console.log('user disconnected');        
+
+    socket.on('chat message', async (msg) => {
+        try {
+            // Insert the message into the Supabase database
+            const { data, error } = await supabase
+                .from('messages')
+                .insert([{ message: msg }]);
+
+            if (error) throw error;
+
+            // Optionally, emit the message or a confirmation back to the client
+            io.emit('chat message', msg);
+        } catch (error) {
+            console.error('Error inserting message:', error);
+        }
     });
-    socket.on('chat message', (msg) => {
-        console.log('message: ' + msg);
-        io.emit('chat message', msg);
-      });
-  })
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
+
+
 app.use(loger('dev'));
 
 
